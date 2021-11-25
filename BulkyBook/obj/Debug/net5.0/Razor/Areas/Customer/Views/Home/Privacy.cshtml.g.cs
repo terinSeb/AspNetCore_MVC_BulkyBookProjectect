c839aeb4b0b201be0f1621bc34dc0346a53b577a
Terin Sebastin <terin01@gmail.com>
2021-11-25 07:46:29 +0530

    Creating Admin Area and configuring Viewstart and ViewImports

--- BulkyBook/obj/Debug/net5.0/Razor/Areas/Customer/Views/Home/Privacy.cshtml.g.cs
+++ BulkyBook/obj/Debug/net5.0/Razor/Areas/Customer/Views/Home/Privacy.cshtml.g.cs
@@ -12,7 +12,22 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "D:\AspNetCoreMVC_BulkyBookProject\BulkyBook\BulkyBook\Areas\Customer\Views\_ViewImports.cshtml"
+using BulkyBook;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "D:\AspNetCoreMVC_BulkyBookProject\BulkyBook\BulkyBook\Areas\Customer\Views\_ViewImports.cshtml"
+using BulkyBook.Models.ViewModels;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d8ddb6bffa5a9b264bf8f89038bf03c234083fd3", @"/Areas/Customer/Views/Home/Privacy.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"772f79224864f71b3f583e984145299dbe75211c", @"/Areas/Customer/Views/_ViewImports.cshtml")]
     public class Areas_Customer_Views_Home_Privacy : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
