7304b13dfb490d4cb1626997dd2f7a685f43bd08
Terin Sebastin <terin01@gmail.com>
2021-11-30 07:52:21 +0530

    Added Category Table

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,8 @@ namespace BulkyBook.DataAccess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        {
+        {             
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
