fcc1bb7c6d4b0788bf9b93801686b9cdf26fce50
Terin Sebastin <terin01@gmail.com>
2021-12-21 07:58:48 +0530

    Adding Category.js file

---
+++ BulkyBook/wwwroot/js/category.js
@@ -0,0 +1,31 @@
+﻿let dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Category/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return
+                    `
+                        <div class="text-center">
+    <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer;">
+        <i class="fas fa-edit"></i> &nbsp;
+    </a>
+    <a  class="btn btn-success text-white" style="cursor:pointer;">
+        <i class="fas fa-trash"></i> &nbsp;
+    </a>
+</div>
+
+                    `;
+                }, "width": "40%"
+            }
+        ]
+    })
+}
