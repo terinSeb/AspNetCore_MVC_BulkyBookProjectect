acc1dc9f4821effae0c6fd9be290f632821f50cc
Terin Sebastin <terin01@gmail.com>
2022-03-16 07:53:42 +0530

    Added User Controller

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ApplicationDbContext _db;
+        public UserController(IUnitOfWork unitOfWork,ApplicationDbContext db)
+        {
+            _unitOfWork = unitOfWork;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        public IActionResult GetAll()
+        {
+           var userList =  _db.ApplicationUsers.Include(u => u.company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach(var user in userList)
+            {
+
+            }
+            return Json(new { data = userList });
+        }
+        #endregion
+    }
+}
